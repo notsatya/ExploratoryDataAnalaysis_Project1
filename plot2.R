8d2e38fd72599fd7f2a0db15e0d5fbb49075ba82
notsatya <notsatya@gmail.com>
2016-02-21 19:06:19 -0800

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -8,7 +8,7 @@ data <-subset(alldata,Date >= "2007-02-01" & Date <= "2007-02-02")
 date_time <- paste(data$Date, data$Time)
 data$Datetime <- as.POSIXct(date_time)
 
-#--------------plot2
+#Code to create plot2
 plot(data$Datetime, data$Global_active_power,type="l",xlab="",ylab="Global Active Power (kilowatts)")
 dev.copy(png, file="plot2.png", height=480, width=480)
 dev.off()
